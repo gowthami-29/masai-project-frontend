d83ea233b3e2fe47abf0810daa17e4f587407ac4
gowthami-29 <gowthamikanchi29@gmail.com>
2026-02-26 21:45:32 +0530

    j

--- src/services/incomeService.js
+++ src/services/incomeService.js
@@ -1,34 +1,38 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
-const API = `${BASE_URL}/api/income`
+const API = 
+"https://fintrack-api-wn7l.onrender.com/api/income";
 
-const getToken = () => localStorage.getItem("token")
+const getToken = () => localStorage.getItem("token")?.trim()
 
+// ADD
 export const addIncome = (data) =>
   axios.post(API, data, {
     headers: {
       Authorization: `Bearer ${getToken()}`
     }
-  })
+  });
 
+// GET
 export const getIncome = () =>
   axios.get(API, {
     headers: {
       Authorization: `Bearer ${getToken()}`
     }
-  })
+  });
 
+// UPDATE
 export const updateIncome = (id, data) =>
   axios.put(`${API}/${id}`, data, {
     headers: {
       Authorization: `Bearer ${getToken()}`
     }
-  })
+  });
 
+// DELETE
 export const deleteIncome = (id) =>
   axios.delete(`${API}/${id}`, {
     headers: {
       Authorization: `Bearer ${getToken()}`
     }
-  })
+  });
