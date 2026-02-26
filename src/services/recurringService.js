c00b5de8187e058c453a2d0954a2b6f2db28721b
gowthami-29 <gowthamikanchi29@gmail.com>
2026-02-26 22:25:37 +0530

    kddddd

--- src/services/recurringService.js
+++ src/services/recurringService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://fintrack-api-wn7l.onrender.com/api/recurring";
+const API = "https://fintrack-api-wn7l.onrender.com/api/recuring";
 
 const getToken = () => localStorage.getItem("token");
 
